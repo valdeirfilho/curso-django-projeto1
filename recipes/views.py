99323d50ff2d953c6a4e9a3cf4f73d3486a3b8f1
valdeirfilho <valdeirfilho@msn.com>
2022-02-09 14:05:31 -0300

    organização das urls em um arquivo

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('home')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
