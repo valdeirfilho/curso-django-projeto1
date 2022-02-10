c9dcd1dbafc4a484b8bccbf0ca47ff8acc388ebf
valdeirfilho <valdeirfilho@msn.com>
2022-02-10 09:46:44 -0300

    Preparação para arquivos estáticos do site

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name' : 'Valdeir Filho',
     })
 
-def contato(request):
-    return HttpResponse('contato')
 
-def sobre(request):
-    return HttpResponse('sobre')
 
