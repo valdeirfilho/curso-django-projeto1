42eb130460eaa132e944bc9fe361318a63784835
Valdeir Filho <valdeirfilho@msn.com>
2022-03-10 16:39:30 -0300

    Resolvido problema do CSS na página de receitas.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from recipes.views import home
-from recipes import views
-#  from . import views
 
+from . import views
 
 urlpatterns = [
-    path('', views.home), #Home
-    path('recipes/<int:id>/', views.recipe), #Home
-] 
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name' : 'Valdeir Filho',
+        'name': 'Luiz Otávio',
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name' : 'Valdeir Filho',
+        'name': 'Luiz Otávio',
     })
