f3c665267aee073b05d26cba9045302a24568f9c
valdeirfilho <valdeirfilho@msn.com>
2022-04-05 15:47:36 -0300

    Erro de exibição de imagens

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        # 'recipes': [make_recipe() for _ in range(10)], #geração de dados aleatórios via lib faker
+        'recipes': recipes
     })
 
 
